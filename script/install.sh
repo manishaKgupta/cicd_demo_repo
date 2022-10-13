0997537d62893354a39379db221152716787581b
manisha <guptamanisha3845@gmail.com>
2022-10-13 12:29:11 +0530

    Adding permisions for stop.sh file

--- script/install.sh
+++ script/install.sh
@@ -5,4 +5,4 @@ sudo yum install nginx -y
 sudo systemctl enable nginx
 sudo systemctl start nginx
 sudo yum install php php-fpm php-mysql php-curl php-gd php-mbstring php-xml php-xmlrpc php-soap php-intl php-zip -y
-sudo chmod 600 stop.sh
+sudo chmod 600 ./script/stop.sh
